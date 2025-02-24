1e0d09b8a69033d2f28ef970781f2d972d684e56
finn-rules <finn.rowles@gmail.com>
2025-02-23 22:51:45 -0600

    Update GapBuffer.java

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -1,38 +1,127 @@
 package edu.grinnell.csc207.texteditor;
-
+import java.util.Arrays;
 /**
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+    public int arrSize;
+    public int gapStart;
+    public int gapEnd;
+    int numElements;
+    public char[] arr;
+    /**
+     * Constructor for the gap buffer.
+     */
+    public GapBuffer() {
+       this.arrSize = 1;
+       this.numElements = 0;
+       this.gapStart = 0;
+       this.gapEnd = 1;
+       this.arr = new char[arrSize];
+    }
+    
+    /**
+     * Insert : to insert a character
+     * @param ch : the character to insert.
+     */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (gapStart >= gapEnd) {
+            System.out.println("Expanding!");
+            arrSize *= 2;
+            char[] temp = Arrays.copyOf(arr, arrSize);
+            for (int i = gapEnd + arrSize / 2; i < arrSize; i++) {
+                temp[i] = arr[i - arrSize / 2];
+            }
+            this.arr = temp;
+            gapEnd = gapStart + (arrSize / 2);
+            System.out.println("End index: " + gapEnd);
+        }
+        System.out.println("Attempting to place. Start index: " + gapStart);
+        arr[gapStart] = ch;
+        gapStart++;
+        numElements++;
     }
 
+    /**
+     * delete: deletes the character at the start of the gap.
+     */
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if(gapStart != 0 && arrSize != 0) {
+            arrSize--;
+            gapStart--;
+            numElements--;
+        }
     }
 
+    /**
+     * getCursorPosition
+     * @return the position of the start of the gap
+     */
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return this.gapStart;
     }
 
+    /**
+     * moveLeft: moves the entire gap left
+     */
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        if (gapStart > 0) {
+            this.gapStart--;
+            this.gapEnd--; 
+            this.arr[gapEnd] = this.arr[gapStart];         
+        }
     }
-
+    
+    /**
+     * moveRight: moves the entire gap right
+     */
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        if (gapEnd < arr.length - 2) {
+            this.gapStart++;
+            this.gapEnd++; 
+            this.arr[gapStart] = this.arr[gapEnd];         
+        }
     }
 
+    /**
+     * getSize: returns the number of "occupied" elements in the buffer.
+     * @return int
+     */
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return this.numElements;
     }
-
-    public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+    
+    /**
+     * getChar gets the character in the buffer at index i
+     * @param i for index
+     * @return char: the character at index i
+     * @throws IndexOutOfBoundsException 
+     */
+    public char getChar(int i) throws IndexOutOfBoundsException { 
+    // note: may not get the correct character due to not considering buffer
+        if(i > arrSize - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return this.arr[i];
     }
-
+    
+    /**
+     * toString: converts the buffer (no gap) to a string.
+     * @return the String. 
+     */
+    @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String s = "";
+        int totalStringSize = this.numElements;
+        for (int i = 0; i < gapStart; i++) {
+            s = s + this.arr[i];
+            totalStringSize--;
+        }
+        if (totalStringSize > 0) {
+            for (int i = 0; i < totalStringSize; i++) {
+                s = s + this.arr[gapEnd + i];
+            }
+        }
+        return s;
     }
 }
